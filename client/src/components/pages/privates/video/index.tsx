f1f7a4c5186b573dd893056fe6ddda269c915f1a
parkyes90 <parkyes90@gmail.com>
2020-12-18 20:56:36 +0900

    feat(stream): Add media connect

--- client/src/components/pages/privates/video/index.tsx
+++ client/src/components/pages/privates/video/index.tsx
@@ -1,6 +1,36 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 const Video: React.FC = () => {
-  return <div>Video</div>;
+  const local = useRef<HTMLVideoElement>(null);
+  const remote = useRef<HTMLVideoElement>(null);
+  useEffect(() => {
+    navigator.getUserMedia(
+      { audio: true, video: { width: 1280, height: 720 } },
+      (stream) => {
+        if (local.current) {
+          console.log(stream);
+          local.current.srcObject = stream;
+          local.current.play().then();
+        }
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }, []);
+  return (
+    <div>
+      <div>
+        <div>Local</div>
+        <div>
+          <video ref={local} />
+        </div>
+      </div>
+      <div>Remote</div>
+      <div>
+        <video ref={remote} />
+      </div>
+    </div>
+  );
 };
 
 export default Video;
