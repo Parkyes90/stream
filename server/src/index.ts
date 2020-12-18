9033c8bfdd1ff98f4742bf1ddfe41c6d51414008
parkyes90 <parkyes90@gmail.com>
2020-12-18 21:48:17 +0900

    chore(stream): fix bugs

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
-import { Server } from "socket.io";
+const io = require("socket.io");
+
 const app = express();
 const server = app.listen(8000, function () {
   console.log("Listening to Port 8000");
@@ -8,28 +9,24 @@ type Message = {
   username: string;
   message: string;
 };
-const socketServer = new Server(server);
+const socketServer = io(server);
 
-socketServer.on("connection", (socket) => {
+socketServer.on("connection", (socket: any) => {
   socket.on("sendingMessage", (data: Message) => {
     socketServer.emit("broadcastMessage", data);
   });
   socket.on("join", (roomName: string) => {
-    const rooms = socketServer.sockets.adapter.socketRooms(socket.id);
-    const guests = socketServer.sockets.adapter.sockets(new Set(roomName));
-    guests
-      .then((res) => {
-        console.log(res);
-        if (res.size < 2) {
-          socket.join(roomName);
-          console.log(`Room ${res.size === 0 ? "Created" : "Joined"}`);
-        } else {
-          console.log("Room Full for Now");
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    const rooms = socketServer.sockets.adapter.rooms;
+    const room = rooms.get(roomName);
+    if (!room) {
+      socket.join(roomName);
+      console.log(`Room "Created"`);
+    } else if (room.size === 1) {
+      socket.join(roomName);
+      console.log(`Room "Joined"}`);
+    } else {
+      console.log(`Room Full for Now"}`);
+    }
 
     console.log(rooms);
   });
