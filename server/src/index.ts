0e0612aab4e9dca632f39ef2f063c69e92e90b10
parkyes90 <parkyes90@gmail.com>
2020-12-19 15:50:05 +0900

    feature(stream): Add signaling

--- server/src/index.ts
+++ server/src/index.ts
@@ -20,16 +20,31 @@ socketServer.on("connection", (socket: any) => {
     const room = rooms.get(roomName);
     if (!room) {
       socket.join(roomName);
-      console.log(`Room "Created"`);
+      socket.emit("created");
     } else if (room.size === 1) {
       socket.join(roomName);
-      console.log(`Room "Joined"}`);
+      socket.emit("joined");
     } else {
-      console.log(`Room Full for Now"}`);
+      socket.emit("full");
     }
 
     console.log(rooms);
   });
-
+  socket.on("ready", (roomName: string) => {
+    console.log("Ready");
+    socket.broadcast.to(roomName).emit("ready");
+  });
+  socket.on("candidate", (candidate: any, roomName: string) => {
+    console.log("Candidate");
+    socket.broadcast.to(roomName).emit("candidate", candidate);
+  });
+  socket.on("offer", (offer: any, roomName: string) => {
+    console.log("Offer");
+    socket.broadcast.to(roomName).emit("offer", offer);
+  });
+  socket.on("answer", (answer: any, roomName: string) => {
+    console.log("Answer");
+    socket.broadcast.to(roomName).emit("answer", answer);
+  });
   console.log("Websocket Connected", socket.id);
 });
