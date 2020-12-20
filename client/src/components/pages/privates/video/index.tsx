159e60228b839b89c4353eb6001e52c93db84806
parkyes90 <parkyes90@gmail.com>
2020-12-20 16:23:22 +0900

    feature(stream): Add RTCPeerConnection and ICE

--- client/src/components/pages/privates/video/index.tsx
+++ client/src/components/pages/privates/video/index.tsx
@@ -1,9 +1,28 @@
 import React, { useEffect, useRef } from "react";
 import { socket } from "../../../../index";
+
 const Video: React.FC = () => {
   const local = useRef<HTMLVideoElement>(null);
   const remote = useRef<HTMLVideoElement>(null);
   useEffect(() => {
+    const roomName = "video";
+    let created = false;
+    let rtcPeerConnection: RTCPeerConnection;
+    const iceServers = {
+      iceServers: [
+        {
+          urls: "stun:stun.services.mozilla.com",
+        },
+        {
+          urls: "stun1.1.google.com:19302",
+        },
+      ],
+    };
+    const onIceCandidate = (event: any) => {
+      if (event.candidate) {
+        socket.emit("candidate", event.candidate, roomName);
+      }
+    };
     const connectMedia = () => {
       navigator.mediaDevices
         .getUserMedia({ audio: true, video: { width: 1280, height: 720 } })
@@ -17,15 +36,23 @@ const Video: React.FC = () => {
           console.log(error);
         });
     };
-    socket.emit("join", "video");
+    socket.emit("join", roomName);
     socket.on("created", () => {
+      created = false;
       connectMedia();
     });
     socket.on("joined", () => {
+      created = true;
       connectMedia();
     });
     socket.on("full", () => {
       window.alert("Room is Full");
+    });
+    socket.on("ready", () => {
+      if (created) {
+        rtcPeerConnection = new RTCPeerConnection(iceServers);
+        rtcPeerConnection.onicecandidate = onIceCandidate;
+      }
     });
     socket.on("candidate", () => {});
     socket.on("offer", () => {});
